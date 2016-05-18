6226b8a608316172a4a64f43186b32fd1483ab80
Eric <mlynare@gmail.com>
2016-05-18 19:53:53 -0400

    this is my latest try.

--- TestOne/TestOne/Program.cs
+++ TestOne/TestOne/Program.cs
@@ -10,7 +10,21 @@ namespace TestOne
     {
         static void Main(string[] args)
         {
-            // Hey Will, can you see this?
+            // Create List
+            List<Post> posts = new List<Post>();
+
+            // Populate List
+            for (int i = 0; i < 10; i++)
+            {
+                Post j = new Post();
+                j.Title = "The Best Movie";
+                j.Description = "This movie will bring you to tears";
+                j.PostTime = DateTime.Now;
+                posts.Add(j);
+            }
+
+            // Display List
+            
         }
     }
 }
---
+++ TestOne/TestOne/post.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestOne
+{
+    class Post
+    {
+        private string title;
+        private string description;
+        private DateTime postTime;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+        public DateTime PostTime
+        {
+            get
+            {
+                return postTime;
+            }
+
+            set
+            {
+                postTime = value;
+            }
+        }
+    }
+}
